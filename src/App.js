34f9b5713a9f1a3a7669216c0e56e1a00c60eb72
MuratFaizulla <130447207+MuratFaizulla@users.noreply.github.com>
2024-01-28 22:37:05 +0600

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table cellPadding="20">
+        <tr>
+          <td>
+          <img src="./src/photo.jpg" alt="Faizulla Murat" width="100" style={{ borderRadius: '50%' }} />
+          </td>
+          <td>
+            <h1>Faizulla Murat</h1>
+            <p><em>A Student at <a href="https://enu.kz/ru/" target="_blank" rel="noopener noreferrer">L.N. Gumilyov Eurasian National University</a></em></p>
+            <p>I'm a Front-End Developer and a passionate student interested in web development and learning.</p>
+          </td>
+        </tr>
+      </table>
+      <ul>
+        <strong>Eurasian National University, Bachelor, 2020-2024</strong>
+        <li>Computer Engineering and Software, GPA-3.17</li>
+      </ul>
     </div>
   );
 }
